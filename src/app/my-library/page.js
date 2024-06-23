30cb229ff6f79088e3868d1d8c676eea4cf89bc6
Aamirah <aamirahpatel@hotmail.co.uk>
2024-06-23 19:54:06 +0100

    isbn component

--- src/app/my-library/page.js
+++ src/app/my-library/page.js
@@ -1,8 +1,10 @@
 import MyLibrary from "../components/MyLibrary";
+import SearchBooks from "../components/searchBook";
 
 export default function MyLibraryPage() {
   return (
     <div>
+      <SearchBooks />
       <MyLibrary />
     </div>
   );
